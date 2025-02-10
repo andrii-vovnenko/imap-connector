72c424d469c844a7b269242515e76b66fcac95e2
a.vovnenko <vovnenkoandrii@gmail.com>
2025-02-11 00:06:27 +0100

    add eval

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello, World!");
+import repl from 'repl';
+
+const myAPI = {
+  time: () => `Current time: ${new Date().toLocaleTimeString()}`,
+};
+
+const replServer = repl.start({
+  prompt: "> ",
+  eval: (cmd, _context, _filename, callback) => {
+    console.log(`Received command: ${cmd}`);
+    try {
+      const result = eval(cmd); // Execute command
+      callback(null, result);
+    } catch (err) {
+      callback(err as Error, null);
+    }
+  },
+  writer: (output) => `${output}`, // Format output
+});
